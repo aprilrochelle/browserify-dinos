14fc717b055f4ce86258927c440b9c11034df14d
April Nichols <nicholsar@gmail.com>
2018-04-10 21:38:22 -0500

    wip

---
+++ javascripts/dom.js
@@ -0,0 +1,19 @@
+const outputDiv = document.getElementById('dinos');
+
+const domString = (dinos) => {
+  let strang = '';
+  dinos.forEach((dino) => {
+    strang += `<div class="dinos">`;
+    strang += `<h2>${dino.type}</h2>`;
+    strang += `<img src="${dino.img}">`;
+    strang += `</div>`;
+  });
+  return strang;
+};
+
+const printToDom = (dinoArray) => {
+  outputDiv.innerHTML = domString(dinoArray);
+  domEvents();
+};
+
+module.exports = printToDom;
